01c0f1b067cc3a0733d35f9527186b358acc3efb
Zach Pfeifer <mr.zman1@yahoo.com>
2019-08-16 15:34:21 -0600

    Starting giveItems

--- game.js
+++ game.js
@@ -1,33 +1,59 @@
-let health = 100
-let name = "Demon Knight"
-let hits = 0
+// let health = 100
+// let name = "Demon Knight"
+// let hits = 0
+let target = {
+  name: 'Demon Knight',
+  health: 100,
+  hits: 0,
+  item: []
+}
+var items = {
+  healthPosion: {
+    name: 'Estus Flask',
+    modifier: 5,
+    description: 'Drink to heal your HP for +5.'
+  },
+  throwAble: {
+    name: 'Fire Bomb',
+    modifier: 10,
+    description: 'Burns Target for 10 damage.'
+  },
+  weaponMod: {
+    name: 'Charcoal Pine Resin',
+    modifier: 6,
+    description: 'Applies fire damage to weapon. Gives +6 attacks'
+  },
+}
 
 function slap() {
-  health -= 1
-  hits += 1
+  target.health -= 1
+  target.hits += 1
 
   update()
 }
 function punch() {
-  health -= 5
-  hits += 1
+  target.health -= 5
+  target.hits += 1
 
   update()
 }
 function kick() {
-  health -= 10
-  hits += 1
+  target.health -= 10
+  target.hits += 1
 
   update()
+}
+function giveHealthPosions() {
+
 }
 
 function update() { // This will be responsible for updating the user interface whenever a value changes.
   //let healthElem = document.getElementById('#health').innerText;
   let healthElem = document.querySelector('#health');
-  healthElem.textContent = health.toString();
+  healthElem.textContent = target.health.toString();
   let nameElem = document.querySelector('#name');
-  nameElem.textContent = name;
+  nameElem.textContent = target.name;
   let hitsElem = document.querySelector('#hits');
-  hitsElem.textContent = hits.toString();
+  hitsElem.textContent = target.hits.toString();
 }
 update()
